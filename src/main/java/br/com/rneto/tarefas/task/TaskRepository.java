109a89433dbf85def1b428a4f6ab5a80921610b8
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-08-14 14:48:32 -0300

    feat: adicionando obtencao e atualizacao de tasks

--- src/main/java/br/com/rneto/tarefas/task/TaskRepository.java
+++ src/main/java/br/com/rneto/tarefas/task/TaskRepository.java
@@ -2,8 +2,11 @@ package br.com.rneto.tarefas.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+
 
 }
